5d10fa0cef540f3d2d9ae3c6fb87befa4501d117
Dwatchon <49249709+Dwatchon@users.noreply.github.com>
2022-05-29 00:18:00 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import psycopg2
 
 
 
-list1 = ["Frosti", "Toyota", "Camry", "2010", 2, 3, 2022, 400.00]
+list1 = ['Harsh', 'Honda', 'Civic', 2015, 2, 3, 2022, 450.00]
+list_as_tuple = tuple(list1)
 
 
 def exec_statement(conn, stmt):
     try:
         with conn.cursor() as cur:
-            cur.execute(stmt)
+            cur.execute(stmt, list1)
             row = cur.fetchone()
             conn.commit()
             if row: print(row[0])
@@ -24,13 +25,16 @@ def main():
     connection = psycopg2.connect(os.environ['DATABASE_URL'])
 
     statements = [
-        # CREATE the messages table
-        
-        # INSERT a row into the messages table
-        "INSERT INTO messages (message) VALUES ('Hello world!')",
-        # SELECT a row from the messages table
-        "SELECT message FROM messages"
+
+        """INSERT INTO coEmissionsTable (username, make, model, modelYear, day, month, year, co2)
+            VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
+        """,
+       
+        """SELECT model FROM coEmissionsTable
+        WHERE username = 'Harsh'"""
     ]
+
+    
 
     for statement in statements:
         exec_statement(connection, statement)
@@ -42,17 +46,7 @@ def main():
 if __name__ == "__main__":
     main()
 
-def addPoggyWoggy(listerino):
-    #dddd
-    # Connect to CockroachDB
-    connection = psycopg2.connect(os.environ['DATABASE_URL'])
 
-    statements = ['''
-    
-    INSERT INTO coEmissionsTable(username, make, model, modelYear, day, month, year, co2)
-    VALUES (%s,%s,%s,%d,%d,%d,%d,%f)
-    
-    
-    ''']
+
 
         
